c80ccbd7f10fc1a91a759c6b80c8e0db8638459c
GH-X <75062201+GH-X@users.noreply.github.com>
2021-03-07 14:22:31 +0800

    Add files via upload

--- trunk/user/smartdns/smartdns.sh
+++ trunk/user/smartdns/smartdns.sh
@@ -431,19 +431,20 @@ if [ "$sdns_address" == "1" ]; then
   cat $ADDRESS_LOG $ADDRESS_CONF | grep -v '^$' | awk -F/ '!a[$2]++{print $0}' | while read line
   do
     echo "$line" >> $ADDRESS_TEMP
-    md5sum $ADDRESS_TEMP >> $ADDRESS_MD5
-    md5sum $ADDRESS_CONF -c $ADDRESS_MD5
-    if [ "$?" == "0" ]; then
-      rm -f $ADDRESS_TEMP
-      rm -f $ADDRESS_MD5
-      logger -t "SmartDNS" "没有新的域名地址"
-    else
-      rm -f $ADDRESS_CONF
-      cp -rf $ADDRESS_TEMP $ADDRESS_CONF
-      rm -f $ADDRESS_TEMP
-      logger -t "SmartDNS" "域名地址更新成功"
-    fi
+    sleep 1s
   done
+  md5sum $ADDRESS_TEMP >> $ADDRESS_MD5
+  md5sum $ADDRESS_CONF -c $ADDRESS_MD5
+  if [ "$?" == "0" ]; then
+    rm -f $ADDRESS_TEMP
+    rm -f $ADDRESS_MD5
+    logger -t "SmartDNS" "没有新的域名地址"
+  else
+    rm -f $ADDRESS_CONF
+    cp -rf $ADDRESS_TEMP $ADDRESS_CONF
+    rm -f $ADDRESS_TEMP
+    logger -t "SmartDNS" "域名地址更新成功"
+  fi
 fi
 }
 
