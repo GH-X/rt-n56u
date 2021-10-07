8d4ace6c67433e55607afe57254066533c44aa51
GH-X <75062201+GH-X@users.noreply.github.com>
2021-10-08 00:15:35 +0800

    更新 SSP
    chnroute update
    限制chnroute.txt小于600K 限制allroute.txt小于3M
    超过限制保留在临时分区不保存到内部存储 重启后恢复默认
    添加allroute.txt自定义链接 命令:nvram set allroute_url=链接

--- trunk/user/chnroute/scripts/update_chnroute.sh
+++ trunk/user/chnroute/scripts/update_chnroute.sh
@@ -4,31 +4,47 @@ set -e -o pipefail
 
 [ "$1" != "force" ] && [ "$(nvram get ss_update_chnroute)" != "1" ] && exit 0
 CHNROUTE_URL="$(nvram get chnroute_url)"
+ALLROUTE_URL="$(nvram get allroute_url)"
 ROUTE_URL="http://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest"
+user_agent="Mozilla/5.0 (X11; Linux; rv:74.0) Gecko/20100101 Firefox/74.0"
 
 [ -x /usr/bin/shadowsocks.sh ] && [ -x /usr/bin/smartdns.sh ] && logger -st "SSP[$$]Update" "开始更新路由表..."
 
-rm -f /tmp/chnroute.txt
-rm -f /tmp/allroute.txt
-
+(rm -f /tmp/chnroute.txt
 if [ -z "$CHNROUTE_URL" ]; then
-	curl -k -s --connect-timeout 5 --retry 3 "$ROUTE_URL" | sed '/\*/d' | \
+	curl -k -s -A "$user_agent" --connect-timeout 5 --retry 3 "$ROUTE_URL" | sed '/\*/d' | \
 	awk -F\| '/CN\|ipv4/ { printf("%s/%d\n", $4, 32-log($5)/log(2)) }' > /tmp/chnroute.txt
 else
-	curl -k -s --connect-timeout 5 --retry 3 -o /tmp/chnroute.txt "$CHNROUTE_URL"
+	curl -k -s --connect-timeout 5 --retry 3 -o /tmp/chnroute.txt "$CHNROUTE_URL" && \
+	sed -i '/^#/d' /tmp/chnroute.txt && sed -i '/^$/d' /tmp/chnroute.txt
 fi
-
-curl -k -s --connect-timeout 5 --retry 3 "$ROUTE_URL" | sed '/\*/d' | \
-awk -F\| '/ipv4/ { printf("%s/%d\n", $4, 32-log($5)/log(2)) }' > /tmp/allroute.txt
-
 [ ! -d /etc/storage/chinadns/ ] && mkdir /etc/storage/chinadns/
-mv -f /tmp/chnroute.txt /etc/storage/chinadns/chnroute.txt
-[ ! -d /etc/storage/ssprules/ ] && mkdir /etc/storage/ssprules/
-mv -f /tmp/allroute.txt /etc/storage/ssprules/allroute.txt
+if [ $(stat -c %s /tmp/chnroute.txt) -lt 600000 ]; then
+	mv -f /tmp/chnroute.txt /etc/storage/chinadns/chnroute.txt
+else
+	rm -f /etc/storage/chinadns/chnroute.txt && \
+	ln -sf /tmp/chnroute.txt /etc/storage/chinadns/chnroute.txt
+fi)&
 
-mtd_storage.sh save &>/dev/null
+(rm -f /tmp/allroute.txt
+if [ -z "$ALLROUTE_URL" ]; then
+	curl -k -s -A "$user_agent" --connect-timeout 5 --retry 3 "$ROUTE_URL" | sed '/\*/d' | \
+	awk -F\| '/ipv4/ { printf("%s/%d\n", $4, 32-log($5)/log(2)) }' > /tmp/allroute.txt
+else
+	curl -k -s --connect-timeout 5 --retry 3 -o /tmp/allroute.txt "$ALLROUTE_URL" && \
+	sed -i '/^#/d' /tmp/allroute.txt && sed -i '/^$/d' /tmp/allroute.txt
+fi
+[ ! -d /etc/storage/ssprules/ ] && mkdir /etc/storage/ssprules/
+if [ $(stat -c %s /tmp/allroute.txt) -lt 3000000 ]; then
+	mv -f /tmp/allroute.txt /etc/storage/ssprules/allroute.txt
+else
+	rm -f /etc/storage/ssprules/allroute.txt && \
+	ln -sf /tmp/allroute.txt /etc/storage/ssprules/allroute.txt
+fi)&
+
+wait
+
+mtd_storage.sh save &>/dev/null && logger -st "SSP[$$]Update" "路由表更新完成"
 
 [ "$(nvram get sdns_enable)" = "1" ] && /usr/bin/smartdns.sh restart &>/dev/null
 [ "$(nvram get ss_enable)" = "1" ] && /usr/bin/shadowsocks.sh restart &>/dev/null
-
-logger -st "SSP[$$]Update" "路由表更新完成"
