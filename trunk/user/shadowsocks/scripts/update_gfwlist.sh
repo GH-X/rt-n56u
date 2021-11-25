68223b3934ca4daa0a1596d08d3d0af573d09f27
GH-X <75062201+GH-X@users.noreply.github.com>
2021-11-25 13:22:17 +0800

    SSP 黑名单文件链接相关代码优化

--- trunk/user/shadowsocks/scripts/update_gfwlist.sh
+++ trunk/user/shadowsocks/scripts/update_gfwlist.sh
@@ -4,18 +4,19 @@ set -e -o pipefail
 
 [ "$1" != "force" ] && [ "$(nvram get ss_update_gfwlist)" != "1" ] && exit 0
 GFWLIST_URL="$(nvram get gfwlist_url)"
+GFWALL_PURL="https://raw.githubusercontent.com/GH-X/rt-n56u/GFWListDomainIP"
 
 [ -x /usr/bin/shadowsocks.sh ] && [ -x /usr/bin/smartdns.sh ] && logger -st "SSP[$$]Update" "开始更新黑名单..."
 
 (rm -rf /tmp/GFWblack.conf
 curl -k -s -o /tmp/GFWblack.conf --connect-timeout 5 --retry 3 \
-${GFWLIST_URL:-"https://raw.githubusercontent.com/GH-X/rt-n56u/GFWListDomainIP/GFWblack.conf"})&
+${GFWLIST_URL:-"$GFWALL_PURL/GFWblack.conf"} && logger -st "SSP[$$]Update" "黑名单域名文件下载完成")&
 (rm -rf /tmp/GFWblackip.conf
 curl -k -s -o /tmp/GFWblackip.conf --connect-timeout 5 --retry 3 \
-https://raw.githubusercontent.com/GH-X/rt-n56u/GFWListDomainIP/GFWblackip.conf)&
+$GFWALL_PURL/GFWblackip.conf && logger -st "SSP[$$]Update" "黑名单地址文件下载完成")&
 (rm -rf /tmp/smartdns_address.conf
 curl -k -s -o /tmp/smartdns_address.conf --connect-timeout 5 --retry 3 \
-https://raw.githubusercontent.com/GH-X/rt-n56u/GFWListDomainIP/smartdns_address.conf)&
+$GFWALL_PURL/smartdns_address.conf && logger -st "SSP[$$]Update" "域名地址文件下载完成")&
 
 wait
 
