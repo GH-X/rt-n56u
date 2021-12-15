9160b292bd5afb72b0bc9b9ed5d17a66ef67fb4c
GH-X <75062201+GH-X@users.noreply.github.com>
2021-12-15 16:53:04 +0800

    SSP chnroute 更新透明代理 删除allroute

--- trunk/user/chnroute/scripts/update_chnroute.sh
+++ trunk/user/chnroute/scripts/update_chnroute.sh
@@ -4,11 +4,11 @@ set -e -o pipefail
 
 [ "$1" != "force" ] && [ "$(nvram get ss_update_chnroute)" != "1" ] && exit 0
 CHNROUTE_URL="$(nvram get chnroute_url)"
-ALLROUTE_URL="$(nvram get allroute_url)"
 ROUTE_URL="http://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest"
 user_agent="Mozilla/5.0 (X11; Linux; rv:74.0) Gecko/20100101 Firefox/74.0"
 
-[ -x /usr/bin/shadowsocks.sh ] && [ -x /usr/bin/smartdns.sh ] && logger -st "SSP[$$]Update" "开始更新路由表..."
+([ -x /usr/bin/shadowsocks.sh ] && [ -x /usr/bin/smartdns.sh ] && \
+logger -st "SSP[$$]Update" "开始更新路由表...")&
 
 (rm -f /tmp/chnroute.txt
 if [ -z "$CHNROUTE_URL" ]; then
@@ -24,22 +24,6 @@ if [ $(stat -c %s /tmp/chnroute.txt) -lt 600000 ]; then
 else
 	rm -f /etc/storage/chinadns/chnroute.txt && \
 	ln -sf /tmp/chnroute.txt /etc/storage/chinadns/chnroute.txt
-fi)&
-
-(rm -f /tmp/allroute.txt
-if [ -z "$ALLROUTE_URL" ]; then
-	curl -k -s -A "$user_agent" --connect-timeout 5 --retry 3 "$ROUTE_URL" | sed '/\*/d' | \
-	awk -F\| '/ipv4/ { printf("%s/%d\n", $4, 32-log($5)/log(2)) }' > /tmp/allroute.txt
-else
-	curl -k -s --connect-timeout 5 --retry 3 -o /tmp/allroute.txt "$ALLROUTE_URL" && \
-	sed -i '/^#/d' /tmp/allroute.txt && sed -i '/^$/d' /tmp/allroute.txt
-fi
-[ ! -d /etc/storage/ssprules/ ] && mkdir /etc/storage/ssprules/
-if [ $(stat -c %s /tmp/allroute.txt) -lt 3000000 ]; then
-	mv -f /tmp/allroute.txt /etc/storage/ssprules/allroute.txt
-else
-	rm -f /etc/storage/ssprules/allroute.txt && \
-	ln -sf /tmp/allroute.txt /etc/storage/ssprules/allroute.txt
 fi)&
 
 wait
