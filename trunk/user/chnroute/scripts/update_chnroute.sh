e5021ce33cb7970bc96be62e325c850519e990d0
GH-X <75062201+GH-X@users.noreply.github.com>
2022-05-12 02:20:35 +0800

    更新 SSP
    添加 节点列表(节点设置)
    添加 自动选择(代理类型)
    dns-forwarder 并入代理(删除单独的管理页面和脚本等)
    删除 SmartDNS 所有修改(包含单独的管理页面和脚本等)
    及其它一些小的修复和调整

--- trunk/user/chnroute/scripts/update_chnroute.sh
+++ trunk/user/chnroute/scripts/update_chnroute.sh
@@ -4,23 +4,23 @@ set -e -o pipefail
 
 [ "$1" != "force" ] && [ "$(nvram get ss_update_chnroute)" != "1" ] && exit 0
 CHNROUTE_URL="$(nvram get chnroute_url)"
-ROUTE_URL="http://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest"
+APNIC_URL="http://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest"
 user_agent="Mozilla/5.0 (X11; Linux; rv:74.0) Gecko/20100101 Firefox/74.0"
 
-[ -x /usr/bin/shadowsocks.sh ] && [ -x /usr/bin/smartdns.sh ] && \
-echo "stop_ams" > /tmp/sspstatus.tmp && logger -st "SSP[$$]Update" "开始更新路由表..."
+logger -st "SSP[$$]Update" "开始更新路由表..."
 
 (rm -f /tmp/chnroute.txt
 if [ -z "$CHNROUTE_URL" ]; then
-	curl -k -s -A "$user_agent" --connect-timeout 5 --retry 3 "$ROUTE_URL" | sed '/\*/d' | \
+	curl -k -s -A "$user_agent" --connect-timeout 5 --retry 3 "$APNIC_URL" | sed '/\*/d' | \
 	awk -F\| '/CN\|ipv4/ { printf("%s/%d\n", $4, 32-log($5)/log(2)) }' > /tmp/chnroute.txt
 else
 	curl -k -s --connect-timeout 5 --retry 3 -o /tmp/chnroute.txt "$CHNROUTE_URL" && \
 	sed -i '/^#/d' /tmp/chnroute.txt && sed -i '/^$/d' /tmp/chnroute.txt
 fi
 [ ! -d /etc/storage/chinadns/ ] && mkdir /etc/storage/chinadns/
-if [ $(stat -c %s /tmp/chnroute.txt) -lt 600000 ]; then
+if [ $(stat -c %s /tmp/chnroute.txt) -lt 1000000 ]; then
 	mv -f /tmp/chnroute.txt /etc/storage/chinadns/chnroute.txt
+	mtd_storage.sh save >/dev/null 2>&1
 else
 	rm -f /etc/storage/chinadns/chnroute.txt && \
 	ln -sf /tmp/chnroute.txt /etc/storage/chinadns/chnroute.txt
@@ -28,4 +28,7 @@ fi)&
 
 wait
 
-/usr/bin/smartdns.sh update &>/dev/null && logger -st "SSP[$$]Update" "路由表更新完成"
+[ "$(nvram get ss_enable)" = "1" ] && echo "daten_stopwatchcat" > /tmp/SSP/sspstatus.tmp && \
+/usr/bin/shadowsocks.sh restart &>/dev/null
+
+logger -st "SSP[$$]Update" "路由表更新完成"
